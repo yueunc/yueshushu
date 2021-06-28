68477a821b1a6bda9c208d8d75ef579014a400b8
v_yuegang <RYWwoaini123>
2021-06-28 20:33:27 +0800

    新建外星人游戏项目

---
+++ alien_invasion/settings.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+class Settings():
+    """储存外星人所有设置的类"""
+
+    def __init__(self):
+        """初始化游戏设置"""
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
